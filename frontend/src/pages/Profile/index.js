36ce88e05d60cda21821b6e0d55904b7eed6bd11
Cristiane <cristiane.trc@gmail.com>
2020-03-28 22:23:16 -0300

    Cadastro de casos

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import api from '../../services/api';
 
 import { FiPower, FiTrash2 } from "react-icons/fi";
@@ -9,6 +9,7 @@ import "./style.css";
 export default function Profile() {
   const [incidents, setIncidents] = useState([]);
 
+  const history = useHistory();
 
   const ongId = localStorage.getItem('ongId');
   const ongName = localStorage.getItem('ongName');
@@ -36,6 +37,11 @@ export default function Profile() {
       alert('Erro ao deletar caso, tente novamente.');
     }
   }
+  function handleLogout() {
+    localStorage.clear();
+
+    history.push('/');
+  }
 
   return (
     <div className="profile-container">
@@ -43,7 +49,7 @@ export default function Profile() {
         <img src={logoImg} alt="Be the hero" />
         <span>Bem vinda, {ongName}</span>
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-        <button type="button">
+        <button onClick={handleLogout} type="button">
           <FiPower size={18} color="#e02041" />
         </button>
       </header>
